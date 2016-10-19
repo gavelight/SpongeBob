55560280e688d2583193b25c88a75a4fcf9a7853
Or Natan <ornatan777@gmail.com>
2016-10-19 16:51:16 +0300

    - I took care of the design for the "branches" views - The blue wrap is now placed in the layout instead of each view

--- src/FinalProject/Data/Migrations/20161018134641_BranchUpdate.Designer.cs
+++ src/FinalProject/Data/Migrations/20161018134641_BranchUpdate.Designer.cs
@@ -8,9 +8,10 @@ using FinalProject.Data;
 namespace FinalProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161018134641_BranchUpdate")]
+    partial class BranchUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
